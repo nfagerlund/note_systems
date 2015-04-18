1a539a6c200b8ca9e5a229acc511dc74a8028a14
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-04-17 21:15:08 -0700

    FMP 1.1 - File modification date May 1, 2006

--- fmp.rb
+++ fmp.rb
@@ -1,29 +1,54 @@
 #!/usr/bin/ruby
-# fmp.rb version 0.9
-# (c) Nick Fagerlund; available under the GNU General Public License
-# <GPL URL>
+# fmp.rb version 1.1
+# (c) 2006 Nick Fagerlund; available under the GNU General Public License
+# http://www.gnu.org/copyleft/gpl.html
 # Implements the Fiendish Master Plan capture-sorting system, which
-# is really not so complicated that it deserves its own name, but is
-# called so anyway for Historical Reasons.
+# is really not so sophisticated that it deserves its own name, but 
+# which has one anyway.
 
-# THIS PROGRAM NEEDS TO BE ALTERED BEFORE YOU USE IT. Change all the paths as appropriate. For some reason, I couldn't get Ruby to expand paths properly on the Mac, so I had to hardcode it to my home directory instead of doing the right thing and pointing it at ~/Lists. Also, you may not be swayed by the precedent of Historical Reasons, and will thus want to have your main note file be called something other than "fiend.txt."
+# Use case: ~/Lists/fiend.txt is a text file that I use for catching
+# notes as I think of them. When I have a note that belongs in a
+# separate file, I can put it on one line that begins with the sequence
+# ^nameoffile. When fmp.rb runs, it removes any lines starting with
+# ^variousfilenames and puts them at the end of their appropriate
+# files. 
 
-# Use case: You have one file that's almost always open for catching notes as you think of them. When you have a note that belongs in a separate file, you put it on one line that begins with the sequence ^nameoffile. When you run fmp.rb to sort the main file, it puts any lines starting with ^variousfilenames at the end of their appropriate files. The beauty of this system is that it does the right thing even if ^thatfile doesn't exist yet, so you can create new collections of notes at whim without having to do anything different. 
+# Why I like this: 
+# -I can append to any file in my Lists folder, but I only have to keep
+# one file open. By combining this with Quicksilver's append triggers,
+# I've got a pretty powerful note-sorting system. 
+# -If ^somefile doesn't exist yet, fmp creates it without any fuss, so I
+# can create new collections of notes at whim without having to do
+# anything different. 
+# -I can import any collection of notes into the system simply by
+# dumping the file into ~/Lists and naming it
+# some_filename_sans_spaces.txt.
 
-# Caveats and gotchas:
-# 1. File names used in start-of-line caret tags can't have any spaces in them. Any other filesystem-legal character ought to be fine. You're in charge of figuring out what's filesystem-legal around your system, and I'm not sure yet what happens when it's fed something perverse.
-# 2. Ruby wants nice, clean Unix line breaks, and will boff up the formatting if it doesn't find them. When I first used this on a copy of my notes files, they had lived on three different operating systems, and the breaks were kind of a mess. So before you start using fmp for the first time, it's a good idea to run your files through something like Josh Aas's LineBreak.app.
+# Caveats and gotchas: 
+# 1. File names used in start-of-line caret tags can't have any spaces
+# in them. Any other filesystem-legal character ought to be fine.
+# You're in charge of figuring out what's filesystem-legal around your
+# system, and I'm not sure yet what happens when it's fed something
+# perverse. Probably something pretty hilarious, especially if you use 
+# a pipe or something.
+# 2. Ruby wants nice, clean Unix line breaks, and will boff up the
+# formatting if it doesn't find them. When I first used this on a copy
+# of my notes files, they had lived on three different operating
+# systems, and the breaks were kind of a mess. So before you start
+# using fmp for the first time, it's a good idea to run your files
+# through something like Josh Aas's LineBreak.app.
 
 fiendTwigs = ''
 
-File.readlines("/Users/nick/Lists/fiend.txt").each do |theLine|
+File.readlines("#{File.expand_path("~/Lists/fiend.txt")}").each do |theLine|
 	if theLine =~ /^\^[Ff][Ii][Ee][Nn][Dd] /
 		fiendTwigs << theLine.sub(/^\^[\S]+ /, '')
+		# I think I had a bad dream about THIS bit of perverse input.
 	elsif theLine =~ /^\^[\S]+ /
-		File.open("/Users/nick/Lists/#{theLine.split(' ', 2)[0].sub(/\^/, '').chomp}.txt", "a") { |leafPile| leafPile.puts(theLine.sub(/^\^[\S]+ /, '')) }
+		File.open("#{File.expand_path("~/Lists/#{theLine.split(' ', 2)[0].sub(/\^/, '').chomp}.txt")}", "a") { |leafPile| leafPile.puts(theLine.sub(/^\^[\S]+ /, '')) }
 	else
 		fiendTwigs << theLine
 	end
 end
 
-File.open("/Users/nick/Lists/fiend.txt", "w") { |fiendMain| fiendMain.puts(fiendTwigs) }
+File.open("#{File.expand_path("~/Lists/fiend.txt")}", "w") { |fiendMain| fiendMain.puts(fiendTwigs) }
