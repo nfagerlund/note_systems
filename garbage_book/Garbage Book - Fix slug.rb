4ff2e14e9190dc139a9a6496563622de5e1b40d0
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-02-16 23:49:59 -0800

    Compress runs of consecutive annoying characters

--- garbage_book/Garbage Book - Fix slug.rb
+++ garbage_book/Garbage Book - Fix slug.rb
@@ -2,12 +2,13 @@
 # Tidy up the slug on Garbage Book pages, and leave other filenames alone
 
 ARGF.set_encoding('utf-8')
-annoying_characters = %r{[\\/:\*\?"<>\|#%$;?\n\r]}
+annoying_characters = %r{[\\/:\*\?"<>\|#%$;?\n\r]+}
+trailing_junk = /[ _\.]+$/
 slug_length = 25
 
 if /^(\d{4}\.\d{2}\.\d{2} \(\d{4}\)) .*.txt$/.match(ENV['BB_DOC_NAME'])
   timestamp = $1
-  slug = ARGF.gets.chomp[0..slug_length].gsub(annoying_characters, '_').sub(/[ _\.]+$/, '')
+  slug = ARGF.gets.chomp.gsub(annoying_characters, '_')[0..slug_length].sub(trailing_junk, '')
   new_name = File.join(File.dirname(ENV['BB_DOC_PATH']), "#{timestamp} #{slug}.txt")
   File.rename(ENV['BB_DOC_PATH'], new_name)
 end
--- garbage_book/Garbage Book - Save page.applescript.js
+++ garbage_book/Garbage Book - Save page.applescript.js
@@ -1,14 +1,14 @@
 // Wow, even as gross as this is, it's like 0.03x as gross as the AppleScript version
 var slugLength = 25;
 var gbookDir = $('~/Library/Mobile Documents/com~apple~CloudDocs/Garbage Book').stringByExpandingTildeInPath.js;
-var annoyingCharacters = /[\\:\/\*\?"<>\|#%\$;\?\n]/g;
+var annoyingCharacters = /[\\:\/\*\?"<>\|#%\$;\?\n]+/g;
 var trailingJunk = /[ _\.]+$/;
 
 var bb = Application('BBEdit');
 var frontDocument = bb.textDocuments[0];
 
 var firstLine = frontDocument.text.lines[0].contents();
-var slug = firstLine.substring(0, slugLength).replace(annoyingCharacters, '_').replace(trailingJunk, '');
+var slug = firstLine.replace(annoyingCharacters, '_').substring(0, slugLength).replace(trailingJunk, '');
 
 var now = new Date();
 var year = now.getFullYear().toString(); // can't has strftime??
