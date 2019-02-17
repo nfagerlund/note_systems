f42fb169631ff9ce72b1c160906355e08cd11c1e
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-02-17 15:09:31 -0800

    Why do I always forget about the camelCase

--- fmp/errata/FMP - Direct append.applescript.js
+++ fmp/errata/FMP - Direct append.applescript.js
@@ -1,8 +1,8 @@
 var app = Application.currentApplication(); // Should be FastScripts
 app.includeStandardAdditions = true;
 
-var fmp_dir = $('~/Library/Mobile Documents/com~apple~CloudDocs/Lists').stringByExpandingTildeInPath.js; // $(string) makes an NSString; have to access its .js property to get a javascript string.
-var dump_tag = 'fiend';
+var fmpDir = $('~/Library/Mobile Documents/com~apple~CloudDocs/Lists').stringByExpandingTildeInPath.js; // $(string) makes an NSString; have to access its .js property to get a javascript string.
+var dumpTag = 'fiend';
 
 var result = app.displayDialog("Append text to FMP; use ^caret-tags to choose a file", {defaultAnswer: '', withTitle: 'Enter some text', buttons: ["Cancel", "Append and open", "Append"], defaultButton: "Append"});
 
@@ -13,17 +13,17 @@ if (result.buttonReturned == 'Append' || result.buttonReturned == 'Append and op
 		var note = caretNote[2];
 	}
 	else {
-		var tag = dump_tag;
+		var tag = dumpTag;
 		var note = result.textReturned;
 	}
-	var file = fmp_dir + '/' + tag + '.txt';
-	
+	var file = fmpDir + '/' + tag + '.txt';
+
 	var fm = $.NSFileManager.defaultManager;
-	
+
 	if (!( fm.fileExistsAtPath( $(file) ) )) {
 		fm.createFileAtPathContentsAttributes( $(file), $(), $() ); // $() with no arguments makes a nil object.
 	}
-	
+
 	var fh = $.NSFileHandle.fileHandleForWritingAtPath( $(file) );
 	fh.seekToEndOfFile;
 	fh.writeData( $( note.replace(/ +\/\/ +/g, "\n") + "\n" ).dataUsingEncoding($.NSUTF8StringEncoding) ); // writeData requires an NSData object, so we have to convert text to an NSString with $() and then convert it to NSData (which needs a specified encoding, accessible by name as a constant in the objc runtime).
