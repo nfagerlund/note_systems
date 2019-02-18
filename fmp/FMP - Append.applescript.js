7ccc47999f0bd3bc710da6b522a4180602ec5119
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-02-18 15:17:57 -0800

    You don't have to check for 'did the user hit cancel,' it'll just die

--- fmp/FMP - Append.applescript.js
+++ fmp/FMP - Append.applescript.js
@@ -3,16 +3,12 @@ app.includeStandardAdditions = true;
 
 var fiend = $('~/Library/Mobile Documents/com~apple~CloudDocs/Lists/fiend.txt').stringByExpandingTildeInPath; // this is an NSString; have to access its .js property to get a javascript string.
 
-var result = app.displayDialog("Append text to " + fiend.js, {defaultAnswer: '', withTitle: 'Enter some text', buttons: ["Cancel", "Append and open", "Append"], defaultButton: "Append"});
+var result = app.displayDialog("Append text to " + fiend.js, {defaultAnswer: '', withTitle: 'Enter some text', buttons: ["Cancel", "Append and open", "Append"], defaultButton: "Append"}); // If you cancel, execution stops here.
 
-if (result.buttonReturned == 'Append' || result.buttonReturned == 'Append and open') {
-	var text = result.textReturned + "\n";
-
-	var fh = $.NSFileHandle.fileHandleForWritingAtPath(fiend);
-	fh.seekToEndOfFile;
-	fh.writeData( $( text ).dataUsingEncoding($.NSUTF8StringEncoding) ); // writeData requires an NSData object, so we have to convert text to an NSString with $() and then convert it to NSData (which needs a specified encoding, accessible by name as a constant in the objc runtime).
-	fh.closeFile;
-}
+var fh = $.NSFileHandle.fileHandleForWritingAtPath(fiend);
+fh.seekToEndOfFile;
+fh.writeData( $( result.textReturned + "\n" ).dataUsingEncoding($.NSUTF8StringEncoding) ); // writeData requires an NSData object, so we have to convert text to an NSString with $() and then convert it to NSData (which needs a specified encoding, accessible by name as a constant in the objc runtime).
+fh.closeFile;
 
 if (result.buttonReturned == 'Append and open') {
 	var bb = Application('BBEdit');
--- fmp/FMP - Direct append.applescript.js
+++ fmp/FMP - Direct append.applescript.js
@@ -4,31 +4,30 @@ app.includeStandardAdditions = true;
 var fmpDir = $('~/Library/Mobile Documents/com~apple~CloudDocs/Lists').stringByExpandingTildeInPath.js; // $(string) makes an NSString; have to access its .js property to get a javascript string.
 var dumpTag = 'fiend';
 
-var result = app.displayDialog("Append text to FMP; use ^caret-tags to choose a file", {defaultAnswer: '', withTitle: 'Enter some text', buttons: ["Cancel", "Append and open", "Append"], defaultButton: "Append"});
+var result = app.displayDialog("Append text to FMP; use ^caret-tags to choose a file", {defaultAnswer: '', withTitle: 'Enter some text', buttons: ["Cancel", "Append and open", "Append"], defaultButton: "Append"}); // If you cancel, execution stops here.
 
-if (result.buttonReturned == 'Append' || result.buttonReturned == 'Append and open') {
-	var caretNote = result.textReturned.match(/^\^([\w\-]+) (.*)$/);
-	if (caretNote) {
-		var tag = caretNote[1].toLowerCase();
-		var note = caretNote[2];
-	}
-	else {
-		var tag = dumpTag;
-		var note = result.textReturned;
-	}
-	var file = fmpDir + '/' + tag + '.txt';
-
-	var fm = $.NSFileManager.defaultManager;
-
-	if (!( fm.fileExistsAtPath( $(file) ) )) {
-		fm.createFileAtPathContentsAttributes( $(file), $(), $() ); // $() with no arguments makes a nil object.
-	}
-
-	var fh = $.NSFileHandle.fileHandleForWritingAtPath( $(file) );
-	fh.seekToEndOfFile;
-	fh.writeData( $( note.replace(/ +\/\/ +/g, "\n") + "\n" ).dataUsingEncoding($.NSUTF8StringEncoding) ); // writeData requires an NSData object, so we have to convert text to an NSString with $() and then convert it to NSData (which needs a specified encoding, accessible by name as a constant in the objc runtime).
-	fh.closeFile;
+var caretNote = result.textReturned.match(/^\^([\w\-]+) (.*)$/);
+if (caretNote) {
+	var tag = caretNote[1].toLowerCase();
+	var note = caretNote[2];
 }
+else {
+	var tag = dumpTag;
+	var note = result.textReturned;
+}
+var file = fmpDir + '/' + tag + '.txt';
+
+// Create empty file if it doesn't exist:
+var fm = $.NSFileManager.defaultManager;
+if (!( fm.fileExistsAtPath( $(file) ) )) {
+	fm.createFileAtPathContentsAttributes( $(file), $(), $() ); // $() with no arguments makes a nil object.
+}
+
+// Write text to file:
+var fh = $.NSFileHandle.fileHandleForWritingAtPath( $(file) );
+fh.seekToEndOfFile;
+fh.writeData( $( note.replace(/ +\/\/ +/g, "\n") + "\n" ).dataUsingEncoding($.NSUTF8StringEncoding) ); // writeData requires an NSData object, so we have to convert text to an NSString with $() and then convert it to NSData (which needs a specified encoding, accessible by name as a constant in the objc runtime).
+fh.closeFile;
 
 if (result.buttonReturned == 'Append and open') {
 	var bb = Application('BBEdit');
