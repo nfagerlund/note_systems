7c8884cf4ce97e4065e329d4dc0107ec86d96ae0
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-02-19 12:33:49 -0800

    Tidy up string quoting

--- fmp/FMP - Direct append.applescript.js
+++ fmp/FMP - Direct append.applescript.js
@@ -1,10 +1,10 @@
 var app = Application.currentApplication(); // Should be FastScripts
 app.includeStandardAdditions = true;
 
-var fmpDir = $('~/Library/Mobile Documents/com~apple~CloudDocs/Lists').stringByExpandingTildeInPath.js; // $(string) makes an NSString; have to access its .js property to get a javascript string.
-var dumpTag = 'fiend';
+var fmpDir = $("~/Library/Mobile Documents/com~apple~CloudDocs/Lists").stringByExpandingTildeInPath.js; // $(string) makes an NSString; have to access its .js property to get a javascript string.
+var dumpTag = "fiend";
 
-var result = app.displayDialog("Append text to FMP; use ^caret-tags to choose a file", {defaultAnswer: '', withTitle: 'Enter some text', buttons: ["Cancel", "Append and open", "Append"], defaultButton: "Append"}); // If you cancel, execution stops here.
+var result = app.displayDialog("Append text to FMP; use ^caret-tags to choose a file", {defaultAnswer: "", withTitle: "Enter some text", buttons: ["Cancel", "Append and open", "Append"], defaultButton: "Append"}); // If you cancel, execution stops here.
 
 var caretNote = result.textReturned.match(/^\^([\w\-]+) (.*)$/);
 if (caretNote) {
@@ -29,8 +29,8 @@ fh.seekToEndOfFile;
 fh.writeData( $( note.replace(/ +\/\/ +/g, "\n") + "\n" ).dataUsingEncoding($.NSUTF8StringEncoding) ); // writeData requires an NSData object, so we have to convert text to an NSString with $() and then convert it to NSData (which needs a specified encoding, accessible by name as a constant in the objc runtime).
 fh.closeFile;
 
-if (result.buttonReturned == 'Append and open') {
-	var bb = Application('BBEdit');
+if (result.buttonReturned == "Append and open") {
+	var bb = Application("BBEdit");
 	bb.activate();
 	bb.open(file);
 }
--- garbage_book/Garbage Book - Save page.applescript.js
+++ garbage_book/Garbage Book - Save page.applescript.js
@@ -1,21 +1,21 @@
 // Wow, even as gross as this is, it's like 0.03x as gross as the AppleScript version
 var slugLength = 25;
-var gbookDir = $('~/Library/Mobile Documents/com~apple~CloudDocs/Garbage Book').stringByExpandingTildeInPath.js;
+var gbookDir = $("~/Library/Mobile Documents/com~apple~CloudDocs/Garbage Book").stringByExpandingTildeInPath.js;
 var annoyingCharacters = /[\\:\/\*\?"<>\|#%\$;\n\r]+/g;
 var trailingJunk = /[ _\.]+$/;
 
-var bb = Application('BBEdit');
+var bb = Application("BBEdit");
 var frontDocument = bb.textDocuments[0];
 
 var firstLine = frontDocument.text.lines[0].contents();
-var slug = firstLine.replace(annoyingCharacters, '_').substring(0, slugLength).replace(trailingJunk, '');
+var slug = firstLine.replace(annoyingCharacters, "_").substring(0, slugLength).replace(trailingJunk, "");
 
 var now = new Date();
 var year = now.getFullYear().toString(); // can't has strftime??
-var month = (now.getMonth() + 1).toString().padStart(2, '0'); // WHY WOULD YOU ZERO-INDEX MONTH
-var day = now.getDate().toString().padStart(2, '0'); // obviously getDay is weekday, obviously
-var hour = now.getHours().toString().padStart(2, '0');
-var minute = now.getMinutes().toString().padStart(2, '0');
+var month = (now.getMonth() + 1).toString().padStart(2, "0"); // WHY WOULD YOU ZERO-INDEX MONTH
+var day = now.getDate().toString().padStart(2, "0"); // obviously getDay is weekday, obviously
+var hour = now.getHours().toString().padStart(2, "0");
+var minute = now.getMinutes().toString().padStart(2, "0");
 var timestamp = year + '.' + month + '.' + day + ' (' + hour + minute + ')';
 
 var newName = gbookDir + '/' + timestamp + ' ' + slug + '.txt';
