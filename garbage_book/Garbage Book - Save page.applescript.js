73623963ba74fba39b890e8b9a67b172b4ddab26
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-02-19 12:47:29 -0800

    mac gbook: isolate the gross date formatting
    and actually, between that and the template literals this script really ain't
    that gross anymore. 🙌🏼

--- garbage_book/Garbage Book - Save page.applescript.js
+++ garbage_book/Garbage Book - Save page.applescript.js
@@ -1,4 +1,4 @@
-// Wow, even as gross as this is, it's like 0.03x as gross as the AppleScript version
+// Save the front BBEdit document with an auto-generated filename
 var slugLength = 25;
 var gbookDir = $("~/Library/Mobile Documents/com~apple~CloudDocs/Garbage Book").stringByExpandingTildeInPath.js;
 var annoyingCharacters = /[\\:\/\*\?"<>\|#%\$;\n\r]+/g;
@@ -10,13 +10,16 @@ var frontDocument = bb.textDocuments[0];
 var firstLine = frontDocument.text.lines[0].contents();
 var slug = firstLine.replace(annoyingCharacters, "_").substring(0, slugLength).replace(trailingJunk, "");
 
-var now = new Date();
-var year = now.getFullYear().toString(); // can't has strftime??
-var month = (now.getMonth() + 1).toString().padStart(2, "0"); // WHY WOULD YOU ZERO-INDEX MONTH
-var day = now.getDate().toString().padStart(2, "0"); // obviously getDay is weekday, obviously
-var hour = now.getHours().toString().padStart(2, "0");
-var minute = now.getMinutes().toString().padStart(2, "0");
-var timestamp = `${year}.${month}.${day} (${hour}${minute})`;
+var timestamp = (() => {
+	let now = new Date();
+	let year = now.getFullYear().toString(); // can't has strftime??
+	let month = (now.getMonth() + 1).toString().padStart(2, "0"); // WHY WOULD YOU ZERO-INDEX MONTH
+	let day = now.getDate().toString().padStart(2, "0"); // obviously getDay is weekday, obviously
+	let hour = now.getHours().toString().padStart(2, "0");
+	let minute = now.getMinutes().toString().padStart(2, "0");
+
+	return `${year}.${month}.${day} (${hour}${minute})`;
+})();
 
 var newName = `${gbookDir}/${timestamp} ${slug}.txt`;
 
