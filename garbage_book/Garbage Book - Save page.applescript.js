257b237736aef56ab9457771aae7ccf646ca8140
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-02-20 11:30:54 -0800

    mac gbook: move all the junk into an iefe
    This is probably super unnecessary, but really it just feels nice
    to show that all this junk is only needed for building that filename and
    can be disregarded otherwise. Very tidy.

--- garbage_book/Garbage Book - Save page.applescript.js
+++ garbage_book/Garbage Book - Save page.applescript.js
@@ -7,20 +7,22 @@ var trailingJunk = /[ _\.]+$/;
 var bb = Application("BBEdit");
 var frontDocument = bb.textDocuments[0];
 
-var firstLine = frontDocument.text.lines[0].contents();
-var slug = firstLine.replace(annoyingCharacters, "_").substring(0, slugLength).replace(trailingJunk, "");
+var newName = (() => {
+    let firstLine = frontDocument.text.lines[0].contents();
+    let slug = firstLine.replace(annoyingCharacters, "_").substring(0, slugLength).replace(trailingJunk, "");
 
-var timestamp = (() => {
-	let now = new Date();
-	let year = now.getFullYear().toString(); // can't has strftime??
-	let month = (now.getMonth() + 1).toString().padStart(2, "0"); // WHY WOULD YOU ZERO-INDEX MONTH
-	let day = now.getDate().toString().padStart(2, "0"); // obviously getDay is weekday, obviously
-	let hour = now.getHours().toString().padStart(2, "0");
-	let minute = now.getMinutes().toString().padStart(2, "0");
+    let timestamp = (() => {
+        let now = new Date();
+        let year = now.getFullYear().toString(); // can't has strftime??
+        let month = (now.getMonth() + 1).toString().padStart(2, "0"); // WHY WOULD YOU ZERO-INDEX MONTH
+        let day = now.getDate().toString().padStart(2, "0"); // obviously getDay is weekday, obviously
+        let hour = now.getHours().toString().padStart(2, "0");
+        let minute = now.getMinutes().toString().padStart(2, "0");
 
-	return `${year}.${month}.${day} (${hour}${minute})`;
+        return `${year}.${month}.${day} (${hour}${minute})`;
+    })();
+
+    return `${gbookDir}/${timestamp} ${slug}.txt`;
 })();
-
-var newName = `${gbookDir}/${timestamp} ${slug}.txt`;
 
 bb.save(frontDocument, {to: Path(newName)}); // JS scripts get to avoid the whole file/alias/POSIX path chaos and just use Path(/posix-y/path).
